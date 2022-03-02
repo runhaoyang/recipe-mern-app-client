8a4a94c10077f9120dfcff154d2d199be532941e
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-01 21:09:55 -0500

    fetch api test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header className="App-header">
+        <button onClick={callApi}>Call API</button>
+      </header>
     </div>
   );
+}
+
+function callApi() {
+  fetch("http://localhost:5000/details", { method: "GET" })
+    .then((data) => data.json())
+    .then((json) => console.log(json));
 }
 
 export default App;

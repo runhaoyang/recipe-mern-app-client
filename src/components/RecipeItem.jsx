3684899210f031040ba9f54f78c9c847efd4cf6c
Runhao Yang <runhao@Runhaos-MacBook-Pro.local>
2022-03-11 20:58:32 -0500

    Added pagination to the recipes page, fixed CSS

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,13 +1,11 @@
 const RecipeItem = ({ item }) => {
   const name = item.strMeal;
   const image = item.strMealThumb;
-  const category = item.strCategory;
 
   return (
     <>
       <div className="recipeItem">
-        <h3 className="recipeItems-name">{name}</h3>
-        {/* <span>{category}</span> */}
+        <p className="recipeItems-name">{name}</p>
         <div className="recipeItems-image-container">
           <img src={image} alt="" />
         </div>
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import Axios from "axios";
 import RecipeItem from "./RecipeItem";
+import Pagination from "./Pagination";
 
 const Recipes = () => {
   const [recipeArray, setRecipeArray] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(8);
 
   useEffect(() => {
     const getAllRecipes = async () => {
@@ -21,16 +24,31 @@ const Recipes = () => {
     console.log(recipeArray);
   };
 
-  const recipes = recipeArray.map((item, index) => {
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentRecipes = recipeArray.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const recipes = currentRecipes.map((item, index) => {
     return <RecipeItem key={index} item={item} />;
   });
 
   return (
     <>
       <div className="recipesHeader">
-        {/* <button onClick={getRecipesArray}>Get recipes array</button> */}
         <div className="recipeItems">{recipes}</div>
       </div>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={recipeArray.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </>
   );
 };
